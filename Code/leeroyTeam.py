7596482de544dd88af0fe1725ea75d668104d877
Michael Rinaldi <mikerinaldi1@yahoo.com>
2016-12-06 22:20:57 -0500

    Using our new getLegalActions for forward looking
    - Descriptive statistics to follow

--- Code/leeroyTeam.py
+++ Code/leeroyTeam.py
@@ -23,6 +23,7 @@ TODOs that will fix this code, currently it loses every time to the baselineTeam
 
 DEBUG = False
 DEFENSE_TIMER_MAX = 100.0
+FORWARD_LOOKING_LOOPS = 1
 
 def createTeam(firstIndex, secondIndex, isRed,
 							 first = 'LeeroyTopAgent', second = 'LeeroyBottomAgent', **args):
@@ -117,12 +118,8 @@ class LeeroyCaptureAgent(ApproximateQAgent):
 		
 		# The total of the legalActions you can take from where you are AND
 		# The legalActions you can take in all future states
-		legalActions = self.getLegalActions(gameState)
-		features['legalActions'] = len(legalActions)
-		for legalAction in legalActions:
-			newState = self.getSuccessor(gameState, legalAction)
-			possibleNewActions = self.getLegalActions(newState)
-			features['legalActions'] += len(possibleNewActions)
+		# It depends on how many loops we do
+		features['legalActions'] = self.getLegalActionModifier(gameState, FORWARD_LOOKING_LOOPS)
 
 		features['backToSafeZone'] = self.getCashInValue(myPos, gameState, myState)
 		
@@ -158,6 +155,15 @@ class LeeroyCaptureAgent(ApproximateQAgent):
 				smallestDist = min(dists)
 				return smallestDist
 		return 0
+
+	def getLegalActionModifier(self, gameState, numLoops):
+		legalActions = self.getLegalActions(gameState)
+		numActions = len(legalActions)
+		for legalAction in legalActions:
+			newState = self.getSuccessor(gameState, legalAction)
+			if numLoops > 0:
+				numActions += self.getLegalActionModifier(newState, numLoops - 1)
+		return numActions
 
 # Leeroy Top Agent - favors pellets with a higher y
 class LeeroyTopAgent(LeeroyCaptureAgent):
